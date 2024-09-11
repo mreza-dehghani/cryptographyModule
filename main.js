35689b054802293be5378636f3664dcc3cf9d9a1
d.sajedi <d.sajedi@fanap.ir>
2024-09-11 10:42:30 +0330

    [implementation] - minor changes

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-import {buildSHA1Hash, buildSHA256Hash, buildSHA384Hash, buildSHA512Hash} from "./src/builder/hash/SHAGenerator.js";
+const moduleWorker = () => {
+    console.log('Crypto Module \n');
+}
 
-console.log(buildSHA1Hash('hello world'));
-console.log(buildSHA512Hash('hello world!'));
-console.log(buildSHA384Hash('hello world!'));
-console.log(buildSHA256Hash('hello world!'));
-
+moduleWorker();
