140af7d9927f36edce16afc97f179e4ce51b0906
d.sajedi <acnt.dv@gmail.com>
2024-09-11 12:43:35 +0330

    [improvements] - minor improvements

--- src/builder/hash/SHAGenerator.js
+++ src/builder/hash/SHAGenerator.js
@@ -1,23 +1,14 @@
 import crypto from 'crypto';
 
-function buildHash(algorithm, string) {
+export const HASHING_ALGORITHM = {
+    SHA1: 'sha1',
+    SHA256: 'sha256',
+    SHA384: 'sha384',
+    SHA512: 'sha512',
+}
+
+export const hashData = (algorithm, string) => {
     const hash = crypto.createHash(algorithm);
     hash.update(string);
     return hash.digest('hex');
-}
-
-export const buildSHA1Hash = (string) => {
-    return buildHash('sha1', string);
-}
-
-export const buildSHA256Hash = (string) => {
-    return buildHash('sha256', string);
-}
-
-export const buildSHA384Hash = (string) => {
-    return buildHash('sha384', string);
-}
-
-export const buildSHA512Hash = (string) => {
-    return buildHash('sha512', string);
 }
