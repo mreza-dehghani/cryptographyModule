4b11195807d2663e4757bc425f4021fd2eae8cea
d.sajedi <acnt.dv@gmail.com>
2024-09-11 14:11:40 +0330

    [completed] - SHA generation implemented and tested

--- main.js
+++ main.js
@@ -4,10 +4,7 @@ import {hashData, HASHING_ALGORITHM} from './src/builder/hash/SHAGenerator.js';
 const moduleWorker = () => {
     try {
         console.log(`Crypto Module ${version}\n`);
-        console.log(`SHA-1 => ${hashData(HASHING_ALGORITHM.SHA1, 'Hello World!')}`);
-        console.log(`SHA-256 => ${hashData(HASHING_ALGORITHM.SHA256, 'Hello World!')}`);
-        console.log(`SHA-384 => ${hashData(HASHING_ALGORITHM.SHA384, 'Hello World!')}`);
-        console.log(`SHA-512 => ${hashData(HASHING_ALGORITHM.SHA512, 'Hello World!')}`);
+
     } catch (error) {
         console.error(error);
     }
