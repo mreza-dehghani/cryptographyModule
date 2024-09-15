89e55e30b0a27c7a62c717ab30ba2e85c90d258f
d.sajedi <acnt.dv@gmail.com>
2024-09-15 15:01:58 +0330

    [improvements] - minor improvements

--- src/builder/utils/readFileContent.js
+++ src/builder/utils/readFileContent.js
@@ -6,5 +6,10 @@ import fs from "node:fs";
  * @returns {string} its buffer of read file
  */
 export const readFileContent = filePath => {
-    return fs.readFileSync(filePath, { encoding: 'utf8' });
+    try {
+        fs.existsSync(filePath)
+    } catch (error) {
+        throw new Error(`Could not read file: ${filePath}\n,Error: ${error}`);
+    }
+    return fs.readFileSync(filePath, {encoding: 'utf8'});
 }
