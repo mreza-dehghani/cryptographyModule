81826c0adc41df66dd5e90085325850e6433ac25
d.sajedi <acnt.dv@gmail.com>
2024-09-15 12:59:05 +0330

    [improvement] - minor improvements

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 import version from './src/version.js'
-import {hashData, HASHING_ALGORITHM} from './src/builder/hash/SHAGenerator.js';
-import {base64ConverterTest, base64Decode, base64Encode} from "./src/builder/utils/converter/base64Converter.js";
-import readline from "node:readline";
 
 const moduleWorker = async () => {
     try {
         console.log(`Crypto Module ${version}\n`);
-        await base64ConverterTest();
 
     } catch (error) {
         console.error(error);
--- src/builder/utils/converter/base64Converter.js
+++ src/builder/utils/converter/base64Converter.js
@@ -1,29 +1,43 @@
 import readline from "node:readline";
 
+/**
+ * encode incoming string or content to base64 string.
+ * @param string origin
+ * @returns {string} base64
+ */
 export const base64Encode = (string) => {
     return Buffer.from(string).toString('base64');
 }
 
+/**
+ * decode base64 string to origin string or content.
+ * @param base64
+ * @returns {string}
+ */
 export const base64Decode = (base64) => {
     return Buffer.from(base64, 'base64').toString('ascii');
 }
 
+/**
+ * simple command line module to test base64 converter.
+ * @returns {Promise<void>}
+ */
 export const base64ConverterTest = async () => {
     try {
 
-        const rl = readline.createInterface({
+        const readLine = readline.createInterface({
             input: process.stdin,
             output: process.stdout
         });
 
-        await rl.question("insert yor string to convert:\n", function (string) {
+        await readLine.question("insert yor string to convert:\n", function (string) {
             const base64 = base64Encode(string);
             console.log(`Original input: ${base64Decode(base64)}`);
             console.log(`Base64 output: ${base64}`);
-            rl.close();
+            readLine.close();
         });
 
-        rl.on("close", function () {
+        readLine.on("close", function () {
             process.exit(0);
         });
     } catch (error) {
