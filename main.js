2b980b8176b8bf1cc7621391e259d19325f5a920
d.sajedi <acnt.dv@gmail.com>
2024-09-15 12:56:08 +0330

    [improvement] - minor improvements

--- main.js
+++ main.js
@@ -1,29 +1,13 @@
 import version from './src/version.js'
 import {hashData, HASHING_ALGORITHM} from './src/builder/hash/SHAGenerator.js';
-import {base64Decode, base64Encode} from "./src/builder/utils/converter/base64Converter.js";
+import {base64ConverterTest, base64Decode, base64Encode} from "./src/builder/utils/converter/base64Converter.js";
 import readline from "node:readline";
 
 const moduleWorker = async () => {
     try {
         console.log(`Crypto Module ${version}\n`);
+        await base64ConverterTest();
 
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-
-        let str;
-        let ans = await rl.question("insert yor string to convert:\n", function (string) {
-            str = string
-            const base64 = base64Encode(string);
-            console.log(`Original input: ${base64Decode(base64)}`);
-            console.log(`Base64 output: ${base64}`);
-            rl.close();
-        });
-
-        rl.on("close", function () {
-            process.exit(0);
-        });
     } catch (error) {
         console.error(error);
     }
--- src/builder/utils/converter/base64Converter.js
+++ src/builder/utils/converter/base64Converter.js
@@ -1,7 +1,32 @@
+import readline from "node:readline";
+
 export const base64Encode = (string) => {
     return Buffer.from(string).toString('base64');
 }
 
 export const base64Decode = (base64) => {
     return Buffer.from(base64, 'base64').toString('ascii');
+}
+
+export const base64ConverterTest = async () => {
+    try {
+
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        await rl.question("insert yor string to convert:\n", function (string) {
+            const base64 = base64Encode(string);
+            console.log(`Original input: ${base64Decode(base64)}`);
+            console.log(`Base64 output: ${base64}`);
+            rl.close();
+        });
+
+        rl.on("close", function () {
+            process.exit(0);
+        });
+    } catch (error) {
+        console.error(error);
+    }
 }
