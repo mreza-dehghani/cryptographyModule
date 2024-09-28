03ccb4466d90dea66ea94d4d3f3cb6468fa2e59d
d.sajedi <acnt.dv@gmail.com>
2024-09-28 12:29:57 +0330

    [improvements] - nothing to share

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 import version from './src/version.js'
-import {encrypt} from "./src/builder/encryption/symmetricEncryption.js";
-import {decrypt} from "./src/builder/decryption/symmetricDecryption.js";
-import crypto from "crypto";
 
 const moduleWorker = async () => {
     try {
