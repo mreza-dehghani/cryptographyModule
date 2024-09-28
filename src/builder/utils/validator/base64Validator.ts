4233fc785b924abe8d3338216ffd306d8bfe5d40
Mohammadreza Dehghani <mohammadreza.dehghani@fanap.ir>
2024-09-28 12:02:16 +0330

    feat: convert utils into "ts"

--- src/builder/utils/validator/base64Validator.ts
+++ src/builder/utils/validator/base64Validator.ts
@@ -3,13 +3,13 @@
  * @param input
  * @returns {boolean}
  */
-export const isValidBase64 = input => {
+export const isValidBase64 = (input: string): boolean => {
     try{
         /**
          * base64 regular expression template
          * @type {RegExp}
          */
-        const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+        const base64regex: RegExp = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
 
         const firstMethodResult = base64regex.test(input);
         const secondMethodResult = atob(input);
